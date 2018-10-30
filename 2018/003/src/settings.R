919cef4a8b89a2e2f9d0fee82a359ba69b7d540a
Mike Sandiford <msandifo@gmail.com>
2018-10-30 11:57:41 +1100

    oct30

--- 2018/003/src/settings.R
+++ 2018/003/src/settings.R
@@ -1,6 +1,7 @@
 if (!exists("full.repro")) full.repro=T
 library(drake)
+library(magrittr)
 pkgconfig::set_config("drake::strings_in_dots" = "literals")
 local.path=NULL
-if (!exists("drake.path")) drake.path <- dirname(rstudioapi::getSourceEditorContext()$path ) 
+if (!exists("drake.path")) drake.path <- dirname(rstudioapi::getSourceEditorContext()$path ) %>% stringr::str_remove("/src")
 setwd(drake.path)
